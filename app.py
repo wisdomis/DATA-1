f827f754f72c913e5e97f28b464edb1691b4cf20
wisdom <kimjihe0220@naver.com>
2023-06-18 17:56:14 +0900

    add : 62 내용/18번까지

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 # app.py
 
 import streamlit as st
-import pandas as pd
-import numpy as np
 #from sklearn.datasets import load_iris 
-from PIL import Image
+#from PIL import Image
 from time import sleep
 
-import streamlit as st
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+from sklearn import tree
+import seaborn as sns
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from math import sqrt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import classification_report
+
+
+
 
 #st.title('데이터보')
 
 DATA_URL = ('')
-
 
 
 
@@ -52,7 +70,119 @@ import datetime
 import streamlit as st
 
 d = st.date_input(
-    "DAY",
+    "TODAY",
     datetime.date(2023, 6, 20))
-st.write('DAY:', d)
+st.write('TODAY:', d)
 
+
+
+
+#추가 사항 (62)
+
+#dedicated_data.hist(figsize=(10,10))
+
+
+#데이터 불러오기
+df = os.getcwd()
+df_lists = os.listdir(df)
+print("Df Lists : ", df_lists)
+
+df1_lists = [f for f in df_lists if f.endswith('.csv')]
+print("Df1 Lists : ", df1_lists)
+
+
+data_lists = [filename for filename in df1_lists if filename != 'kemp-process-rate.csv']
+error_list = 'kemp-process-rate.csv'
+
+print("Data Lists : ", data_lists)
+print("Error Data List : ", error_list)
+
+
+#데이터 조합화
+def csv_read_(data_dir, data_list):
+    tmp = pd.read_csv(os.path.join(data_dir, data_list), sep=',', encoding='UTF8')
+    y, m, d = map(int, data_list.split('-')[-1].split('.')[:-1])
+    time = tmp['Time']
+    tmp['DTime'] = '-'.join(data_list.split('-')[-1].split('.')[:-1])
+    ctime = time.apply(lambda _: _.replace(u'오후', 'PM').replace(u'오전', 'AM'))
+    n_time = ctime.apply(lambda _: datetime.datetime.strptime(_, "%p %I:%M:%S"))
+    newtime = n_time.apply(lambda _: _.replace(year=y, month=m, day=d))
+    tmp['Time'] = newtime
+    return tmp
+
+#x_데이터 합치기
+dd = csv_read_(df, data_lists[0])
+
+for i in range(1, len(data_lists)):
+    dd = pd.merge(dd, csv_read_(df, data_lists[i]), how='outer')
+dd
+
+
+
+process = pd.read_csv(os.path.join(df, error_list), sep=',', encoding='utf-8')
+process
+
+#인덱스 제거
+dd = dd.drop('Index', axis=1)
+dd
+
+#시간->인덱스
+dd = dd.set_index('Time')
+dd
+
+#데이터 복사
+dedicated_data = dd.copy()
+dedicated_data
+
+dedicated_data.columns
+
+dedicated_data.shape
+
+dedicated_data.info()
+
+dedicated_data.isna().sum()
+
+
+#x_데이터 시각화(히스토그램)
+dedicated_data.hist(figsize=(10,10))
+
+
+
+
+# 그래프를 생성합니다
+plt.figure(figsize=(12,6))
+plt.plot(X_data.index, X_data['Process'])
+
+# 축 레이블을 설정합니다
+plt.xlabel('날짜')
+plt.ylabel('품질')
+
+# 날짜 레이블을 자동으로 회전시킵니다
+plt.xticks(rotation=45)
+
+# 그래프를 출력합니다
+plt.show()
+
+
+
+
+
+
+
+
+# 특정 시간 범위 설정
+start_time = pd.to_datetime('2021-09-10 09:01:18')
+end_time = pd.to_datetime('2021-09-10 10:30:00')
+
+# 해당 시간 범위의 데이터 추출
+subset = X_data[(X_data.index >= start_time) & (X_data.index <= end_time)]
+
+# 온도와 품질 그래프
+plt.figure(figsize=(10,5))
+plt.plot(subset.index, subset['pH'], color='tab:blue', label='pH')
+plt.plot(subset.index, subset['Process'], color='tab:red', label='Process')
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.title('Time vs pH/Process')
+plt.legend()
+plt.show()
