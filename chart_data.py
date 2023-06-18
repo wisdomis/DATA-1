e8b6a16c6f833bff93dfb2a5efe5a3388d9fcf04
wisdom <kimjihe0220@naver.com>
2023-06-18 19:31:13 +0900

    수정

--- chart_data.py
+++ chart_data.py
@@ -8,22 +8,29 @@ import streamlit as st
 iris_dataset = load_iris()
 
 df= pd.DataFrame(data=iris_dataset.data,columns= iris_dataset.feature_names)
-df.columns= [ col_name.split(' (cm)')[0] for col_name in df.columns] # 컬럼명을 뒤에 cm 제거하였습니다
-df['species']= iris_dataset.target 
 
 
-species_dict = {0 :'setosa', 1 :'versicolor', 2 :'virginica'} 
-
-def mapp_species(x):
-  return species_dict[x]
-
-
-df['species'] = df['species'].apply(mapp_species)
-print(df)
+#데이터 표시
+df1_lists = [f for f in df_lists if f.endswith('.csv')]
+print("Df1 Lists : ", df1_lists)
 
 
-st.subheader('this is table')
-st.table(df.head())
+#데이터 나누기
+data_lists = [filename for filename in df1_lists if filename != 'kemp-process-rate.csv']
+error_list = 'kemp-process-rate.csv'
 
-st.subheader('this is data frame')
-st.dataframe(df.head())
+print("Data Lists : ", data_lists)
+print("Error Data List : ", error_list)
+
+
+#데이터 조합화
+def csv_read_(data_dir, data_list):
+    tmp = pd.read_csv(os.path.join(data_dir, data_list), sep=',', encoding='UTF8')
+    y, m, d = map(int, data_list.split('-')[-1].split('.')[:-1])
+    time = tmp['Time']
+    tmp['DTime'] = '-'.join(data_list.split('-')[-1].split('.')[:-1])
+    ctime = time.apply(lambda _: _.replace(u'오후', 'PM').replace(u'오전', 'AM'))
+    n_time = ctime.apply(lambda _: datetime.datetime.strptime(_, "%p %I:%M:%S"))
+    newtime = n_time.apply(lambda _: _.replace(year=y, month=m, day=d))
+    tmp['Time'] = newtime
+    return tmp
