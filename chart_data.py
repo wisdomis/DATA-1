2efce3b7ed6ad5792db7ba23c21379c6e12f011f
wisdom <kimjihe0220@naver.com>
2023-06-18 19:24:34 +0900

    add : 콜럼

---
+++ chart_data.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd 
+from sklearn.datasets import load_iris 
+import matplotlib.pyplot as plt
+import streamlit as st
+
+
+iris_dataset = load_iris()
+
+df= pd.DataFrame(data=iris_dataset.data,columns= iris_dataset.feature_names)
+df.columns= [ col_name.split(' (cm)')[0] for col_name in df.columns] # 컬럼명을 뒤에 cm 제거하였습니다
+df['species']= iris_dataset.target 
+
+
+species_dict = {0 :'setosa', 1 :'versicolor', 2 :'virginica'} 
+
+def mapp_species(x):
+  return species_dict[x]
+
+
+df['species'] = df['species'].apply(mapp_species)
+print(df)
