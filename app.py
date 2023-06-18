0530cc2801f3391ec5115be44c3345c86b2be17d
wisdom <kimjihe0220@naver.com>
2023-06-18 18:01:05 +0900

    chore : 주석

--- app.py
+++ app.py
@@ -12,18 +12,18 @@ import matplotlib.pyplot as plt
 import datetime
 #from sklearn import tree
 #import seaborn as sns
-from sklearn.metrics import mean_squared_error
+#from sklearn.metrics import mean_squared_error
 from math import sqrt
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+#from sklearn.metrics import mean_squared_error
+#from sklearn.model_selection import train_test_split
 from math import sqrt
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import roc_curve
-from sklearn.metrics import classification_report
+#from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import accuracy_score
+#from sklearn.metrics import precision_score
+#from sklearn.metrics import recall_score
+#from sklearn.metrics import f1_score
+#from sklearn.metrics import roc_curve
+#from sklearn.metrics import classification_report
 
 
 
